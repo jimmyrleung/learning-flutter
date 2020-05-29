09cadbf32b2fad65a0c25e4a205ac0a68f7640d1
Jimmy Leung <jimmy.rl19@hotmail.com>
2020-05-29 10:28:19 -0300

    stateful widgets

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   // The build method returns the widgets that our custom widget use
   build(BuildContext context) {
     // MaterialApp sets up a lot of basic app configuration (specially the routing config)
@@ -10,9 +19,10 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: Text('Lets see some images!'),
         ),
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
             onPressed: () {
-              print('Hello!');
+              setState(() => counter += 1);
             },
             child: Icon(Icons.add)),
       ), // prevent auto-indenting to a single line
