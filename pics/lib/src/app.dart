861f8a77ed710325d238f33150356cfe279bda54
Jimmy Leung <jimmy.rl19@hotmail.com>
2020-05-30 12:36:31 -0300

    fetching images and mapping to image model

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,4 +1,7 @@
+import 'dart:convert'; //json
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get; // avoid importing the whole package
+import './models/image_model.dart';
 
 class App extends StatefulWidget {
   @override
@@ -8,7 +11,23 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
-  int counter = 0;
+  int counter = 10;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    String url = 'https://jsonplaceholder.typicode.com/photos/$counter';
+
+    // res contains information about the http response (status code, headers, etc.)
+    var res = await get(url);
+    var img = ImageModel.fromJSON(json.decode(res.body));
+
+    print(img);
+
+    setState(() {
+      images.add(img);
+    });
+  }
 
   // The build method returns the widgets that our custom widget use
   build(BuildContext context) {
@@ -20,11 +39,8 @@ class AppState extends State<App> {
           title: Text('Lets see some images!'),
         ),
         body: Text('$counter'),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              setState(() => counter += 1);
-            },
-            child: Icon(Icons.add)),
+        floatingActionButton:
+            FloatingActionButton(onPressed: fetchImage, child: Icon(Icons.add)),
       ), // prevent auto-indenting to a single line
     );
   }
---
+++ pics/lib/src/models/image_model.dart
@@ -0,0 +1,24 @@
+class ImageModel {
+  int id;
+  String url;
+  String title;
+  
+  ImageModel(this.id, this.url, this.title);
+  
+  ImageModel.fromJSON(Map<String, dynamic> parsedJSON) {
+    id = parsedJSON['id'];
+    url = parsedJSON['url'];
+    title = parsedJSON['title'];
+  }
+
+  // shorter form (not recommended, but you better know)
+  // ImageModel.fromJSON(Map<String, dynamic> parsedJSON) :
+  //   id = parsedJSON['id'],
+  //   url = parsedJSON['url'],
+  //   title = parsedJSON['title'];
+
+  @override
+  String toString() {
+    return '$id - $title - $url';
+  }
+}
