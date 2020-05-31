2d0a4c06bbd8cb85f9fd77612a713b6d1edadcb3
Jimmy Leung <jimmy.rl19@hotmail.com>
2020-05-31 10:52:18 -0300

    login stateful project creation

---
+++ login_stateful/lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Log me in!',
+      home: Scaffold(
+        body: Text('Build the login form here!'),
+      )
+    );
+  }
+}
