2430f2aa521b2891b7aa4b1f61b7950575a63f90
Jimmy Leung <jimmy.rl19@hotmail.com>
2020-05-31 22:31:17 -0300

    form validation with global keys and validators

--- login_stateful/lib/src/screens/login_screen.dart
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -8,16 +8,23 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  // Global key to the FormState of our Form
+  final formKey = GlobalKey<FormState>();
+
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20.0),
+
+      // The form field can implicitly identify its children components
+      // Material Form component is a stateful component
       child: Form(
+        key: formKey,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             emailField(),
             passwordField(),
-            
+
             // That container avoid us setting some 'hardcoded' margin into an input
             Container(margin: EdgeInsets.only(top: 15.0)),
             submitButton(),
@@ -34,6 +41,14 @@ class LoginScreenState extends State<LoginScreen> {
         labelText: 'E-mail',
         hintText: 'your@email.com',
       ),
+      validator: (String value) {
+        if(!value.contains('@')) {
+          return 'Please enter a valid e-mail.';
+        }
+
+        // null indicates that the field is valid - it can be omitted
+        return null;
+      },
     );
   }
 
@@ -44,13 +59,22 @@ class LoginScreenState extends State<LoginScreen> {
         labelText: 'Password',
         hintText: 'your strong password',
       ),
+      validator: (String value) {
+        if(value.length < 4) {
+          return 'Password must have at least 4 characters.';
+        }
+
+        return null;
+      },
     );
   }
 
   Widget submitButton() {
     return RaisedButton(
       onPressed: () {
-        print('submit login!');
+        // check all 'validator' property from each children form component
+        final bool isValid = formKey.currentState.validate();
+        print(isValid);
       },
       child: Text('Login'),
       color: Colors.blue,
