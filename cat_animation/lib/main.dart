6f70278e70b293a0c4d48783e758e8aed5397469
Jimmy Leung <jimmy.rl19@hotmail.com>
2020-06-14 16:00:59 -0300

    debug positioning

--- cat_animation/lib/main.dart
+++ cat_animation/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
 
+import 'package:flutter/rendering.dart';
+
 void main() {
+  // uncomment to enable positioning debug
+  // debugPaintSizeEnabled = true;
   runApp(App());
 }
