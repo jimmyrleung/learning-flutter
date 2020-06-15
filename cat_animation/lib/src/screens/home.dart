a60902433e85461a71b098fdb316d06a79ce34ed
Jimmy Leung <jimmy.rl19@hotmail.com>
2020-06-15 16:37:31 -0300

    box flaps with animation

--- cat_animation/lib/src/screens/home.dart
+++ cat_animation/lib/src/screens/home.dart
@@ -9,13 +9,18 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
   Animation<double> catAnimation;
   AnimationController catController;
 
+  Animation<double> boxAnimation;
+  AnimationController boxController;
+
+  final pi = 3.14;
+
   // Lifecycle method
   initState() {
     super.initState();
 
     catController = AnimationController(
-      duration:
-          Duration(milliseconds: 400), // How many seconds the animation will last
+      duration: Duration(
+          milliseconds: 400), // How many seconds the animation will last
       vsync:
           this, // it means that this class is dealing with the animation and has the ticket provider mixin
     );
@@ -30,6 +35,14 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
         curve: Curves.easeIn,
       ),
     );
+
+    boxController = AnimationController(
+      duration: Duration(milliseconds: 250),
+      vsync: this,
+    );
+
+    boxAnimation = Tween(begin: pi * 0.6, end: pi * 0.65)
+        .animate(CurvedAnimation(parent: boxController, curve: Curves.easeInOut));
   }
 
   Widget build(BuildContext context) {
@@ -43,11 +56,12 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
         child: Center(
           child: Stack(
             // align the items inside our stack in the center of this widget
-            alignment: Alignment.center,
             // Pay attention here, the order matters
             children: <Widget>[
               buildCatAnimation(),
               buildBox(),
+              buildLeftFlap(),
+              buildRightFlap(),
             ],
 
             // by default, the stack widget clips any content that overflow
@@ -61,7 +75,7 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
     );
   }
 
-  onTap() {
+  onTap() async {
     if (catController.status == AnimationStatus.completed) {
       //Stopped at the end
       // Reverse the animation
@@ -71,6 +85,11 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       // triggers the animation
       catController.forward();
     }
+
+    // You can do that way or configure an event listener like:
+    // boxAnimation.addStatusListener
+    await boxController.forward();
+    await boxController.reverse();
   }
 
   Widget buildCatAnimation() {
@@ -119,6 +138,66 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
           width: 2.0,
           color: Colors.grey,
           style: BorderStyle.solid,
+        ),
+      ),
+    );
+  }
+
+  Widget buildLeftFlap() {
+    return Positioned(
+      left: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: boxAnimation.value,
+
+            // this is the rotation point: it defaults at the center
+            alignment: Alignment.topLeft,
+            child: child,
+          );
+        },
+        child: Container(
+          height: 10.0,
+          width: 100.0,
+          decoration: BoxDecoration(
+            color: Colors.brown,
+            border: Border.all(
+              width: 2.0,
+              color: Colors.grey,
+              style: BorderStyle.solid,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildRightFlap() {
+    return Positioned(
+      right: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: -boxAnimation.value,
+
+            // this is the rotation point: it defaults at the center
+            alignment: Alignment.topRight,
+            child: child,
+          );
+        },
+        child: Container(
+          height: 10.0,
+          width: 100.0,
+          decoration: BoxDecoration(
+            color: Colors.brown,
+            border: Border.all(
+              width: 2.0,
+              color: Colors.grey,
+              style: BorderStyle.solid,
+            ),
+          ),
         ),
       ),
     );
