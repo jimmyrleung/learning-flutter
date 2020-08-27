1de1427a58db6b4f09381f11b48c90e5ab9e9e0d
Jimmy Leung <jimmy.rl19@hotmail.com>
2020-08-26 22:36:34 -0300

    testing FutureBuilder

--- news/lib/src/screens/news_list.dart
+++ news/lib/src/screens/news_list.dart
@@ -6,7 +6,37 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top news'),
       ),
-      body: Text('News goes here'),
+      body: buildList(),
+    );
+  }
+
+  Widget buildList() {
+      return ListView.builder(
+        itemCount: 1000,
+
+        // represents each item that will be built
+        // this works like a lazy loader because we can render it
+        // conditionally
+        itemBuilder: (context, int index) {
+          return FutureBuilder(
+            future: getFuture(),
+            builder: (context, snapshot) {
+              return Container(
+                height: 80.0,
+                child: snapshot.hasData
+              ? Text("I'm visible, $index")
+              : Text("I haven't fetched data yet $index"),
+              );
+            },
+          );
+        },
+      );
+  }
+
+  Future<String> getFuture() {
+    return Future.delayed(
+      Duration(seconds: 2),
+      () => 'Hi'
     );
   }
 }
